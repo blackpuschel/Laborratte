afdf6a97b3912307f0914b33fec1f6c4c5d9cf3b
Bastian Lehmann <mail@bastian-lehmann.de>
2024-01-05 02:54:07 +0100

    added scaling of ui depending on data (one bug remains)

--- src/ui.py
+++ src/ui.py
@@ -1,28 +1,202 @@
 from math import sqrt
+import struct
+from threading import Thread
+import time
 import pygame
+import socket
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 MAX_FPS = 20
 frame_counter = 0
+IN_HOST = ''
+IN_PORT = 8080
 
-nodes = [
-    (0, 500),
-    (1000, 500),
-    (1000, 300),
-    (1200, 300),
-]
+nodes = []
 
+right_shift = 0
+up_shift = 0
+scale = SCREEN_WIDTH
 
+def shift_nodes(nodes):
+    global right_shift, up_shift
+
+    lowest_x = nodes[0][0]
+    lowest_y = nodes[0][1]
+    for node in nodes:
+        x, y = node[0], node[1]
+        if x < lowest_x:
+            lowest_x = x
+        if y < lowest_y:
+            lowest_y = y
+
+    need_reshift = False
+    new_right_shift = 0
+    if lowest_x + right_shift < 0:
+        new_right_shift = abs(lowest_x)
+        need_reshift = True
+
+    new_up_shift = 0
+    if lowest_y + up_shift < 0:
+        new_up_shift = abs(lowest_y)
+        need_reshift = True
+
+    if need_reshift:
+        reshift_nodes(new_right_shift, new_up_shift)
+
+    shifted_nodes = []
+    for node in nodes:
+        x, y = node[0], node[1]
+
+        shifted_x = x + right_shift
+        shifted_y = y + up_shift
+
+        shifted_nodes.append((shifted_x, shifted_y))
+    return shifted_nodes
+
+def reshift_nodes(new_right_shift, new_up_shift):
+    global nodes, right_shift, up_shift, scale
+
+    rescaled_nodes = []
+    for node in nodes:
+        scaled_x, scaled_y = node[0], node[1]
+
+        scaled_y = abs(scaled_y - SCREEN_HEIGHT)
+
+        shifted_x = scaled_x / scale
+        shifted_y = scaled_y / scale
+
+        x = shifted_x - right_shift
+        y = shifted_y - up_shift
+
+        shifted_x = x + new_right_shift
+        shifted_y = y + new_up_shift
+
+        scaled_x = shifted_x * scale
+        scaled_y = shifted_y * scale
+
+        scaled_y = abs(scaled_y - SCREEN_HEIGHT)
+
+        rescaled_nodes.append((scaled_x, scaled_y))
+
+    right_shift = new_right_shift
+    up_shift = new_up_shift
+    nodes = rescaled_nodes
+
+def scale_nodes(nodes):
+    global scale
+
+    largest_x = 0
+    largest_y = 0
+    for node in nodes:
+        x, y = node[0], node[1]
+        if x > largest_x:
+            largest_x = x
+        if y > largest_y:
+            largest_y = y
+
+    val_x = SCREEN_WIDTH / largest_x
+    val_y = SCREEN_HEIGHT / largest_y
+    new_scale = val_x if val_x < val_y else val_y
+
+    if new_scale < scale:
+        rescale_nodes(new_scale)
+
+    scaled_nodes = []
+    for node in nodes:
+        shifted_x, shifted_y = node[0], node[1]
+
+        scaled_x = shifted_x * scale
+        scaled_y = shifted_y * scale
+
+        scaled_y = abs(scaled_y - SCREEN_HEIGHT)
+
+        scaled_nodes.append((scaled_x, scaled_y))
+    return scaled_nodes
+
+def rescale_nodes(new_scale):
+    global nodes, scale
+
+    scaled_nodes = []
+    for node in nodes:
+        scaled_x, scaled_y = node[0], node[1]
+
+        scaled_y = abs(scaled_y - SCREEN_HEIGHT)
+
+        shifted_x = scaled_x / scale
+        shifted_y = scaled_y / scale
+
+        scaled_x = shifted_x * new_scale
+        scaled_y = shifted_y * new_scale
+
+        scaled_y = abs(scaled_y - SCREEN_HEIGHT)
+
+        scaled_nodes.append((scaled_x, scaled_y))
+
+    scale = new_scale
+    nodes = scaled_nodes
+
+def add_nodes(new_nodes):
+    global nodes
+
+    shifted_nodes = shift_nodes(new_nodes)
+    scaled_nodes = scale_nodes(shifted_nodes)
+    nodes.extend(scaled_nodes)
+    
+def byte_to_nodes(bytes: bytes):
+    result = []
+    for i in range(0, len(bytes), 16):
+        node = struct.unpack('dd', bytes[i:i+16])
+        result.append(node)
+    return result
+
+def data_thread():
+    global nodes
+
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((IN_HOST, IN_PORT))
+            s.listen()
+            print(f"waiting for connection on port {IN_PORT}")
+            conn, addr = s.accept()
+            print("connected")
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    new_nodes = byte_to_nodes(data)
+                    add_nodes(new_nodes)
+                    
+
+thread = Thread(target=data_thread, daemon=True)
+thread.start()
+                    
+CIRCLE_SIZE = 16
+LINE_SPACING = 20
 def drawNodes(animate):
-    pygame.draw.circle(screen, "green", nodes[0], 16, 4)
+    nodes_copy = nodes.copy()
+    if len(nodes_copy) <= 0:
+        return
+    
+    pygame.draw.circle(screen, "green", nodes_copy[0], CIRCLE_SIZE, 4)
     phase = 100
     if animate:
         phase = frame_counter / MAX_FPS * 360
-    for i in range(1, len(nodes)):
-        pygame.draw.circle(screen, "green", nodes[i], 16, 4)
-        drawDashedLine(screen, "green", nodes[i-1], nodes[i], 1, 20, 1, 16, phase)
+    for i in range(1, len(nodes_copy)):
+        pygame.draw.circle(screen, "green", nodes_copy[i], 16, 4)
+
+        node_distance = vector_length(subtract_vectors(nodes_copy[i], nodes_copy[i - 1]))
+        circles_touchin = node_distance < 2 * CIRCLE_SIZE
+        circles_to_close = node_distance < 2 * CIRCLE_SIZE + LINE_SPACING
+
+        if not circles_touchin and circles_to_close:
+            pygame.draw.line(screen, "green", nodes_copy[i-1], nodes_copy[i], 1)
+        elif not circles_touchin:
+            drawDashedLine(screen, "green", nodes_copy[i-1], nodes_copy[i], 1, LINE_SPACING, 1, 16, phase)
 
 def drawDashedLine(surface, color, start_pos, end_pos, width=1, size=1, spacing=1, margin=0, phase=0):
     direction = subtract_vectors(end_pos, start_pos)
